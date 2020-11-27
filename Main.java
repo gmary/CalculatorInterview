e658d7373b0f56a0c33970f6a4f01a99177447e9
Guillaume Mary <guillaume.mary@gmail.com>
2020-11-27 08:35:14 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+import com.neoxam.Calculator;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.internal.TextListener;
+
+class Main {
+  public static void main(String[] args) {
+    JUnitCore junitCore = new JUnitCore();
+		junitCore.addListener(new TextListener(System.out));
+		junitCore.run(Tests.class);
+  }
+
+  public static class Tests {
+
+    @Test
+    public void test() {
+      Calculator cal = new Calculator();
+      assertEquals(cal.add(""), 0);
+    }
+
+  }
+  
+}
---
+++ com/neoxam/Calculator.java
@@ -0,0 +1,19 @@
+package com.neoxam;
+
+/**
+ * Calculator class.
+ */
+public class Calculator {
+
+	/**
+	 * Add numbers contained in string.
+	 * 
+	 * @param numbers
+	 *            numbers string
+	 * @return the sum
+	 */
+	public int add(String numbers) {
+    return 0;
+	}
+
+}
